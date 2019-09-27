d41d7d7549272092bb5671c44a18deb244e7910c
coolernato <cmcooling@gmail.com>
2019-09-27 12:40:19 +0100

    Update

--- tests/test_quadratic_solver.py
+++ tests/test_quadratic_solver.py
@@ -9,3 +9,9 @@ class TestQuadraticSolver(unittest.TestCase):
     def test_zero_a(self):
         self.assertListEqual(quadratic_solver(0, 1, 2), [2])
 
+    def test_single_root(self):
+        result=quadratic_solver(900,300,25)
+
+        self.assertEqual(len(result), 1)
+        self.assertAlmostEqual(result[0], 1/6)
+
