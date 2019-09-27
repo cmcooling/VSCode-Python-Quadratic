c798308c69dd5c00eef09b6c52e7ec2776aa10ce
coolernato <cmcooling@gmail.com>
2019-09-27 14:25:40 +0100

    Added comments

--- tests/test_quadratic_solver.py
+++ tests/test_quadratic_solver.py
@@ -1,14 +1,19 @@
 import unittest
 from quadratic_solver import quadratic_solver
 
+##@class TestQuadraticSolver
+#A class containing tests for the quadratic_solver function
 class TestQuadraticSolver(unittest.TestCase):
+    ##Tests if the quadratic and linear coefficients being zero causes a ValeError to be raised
     def test_zero_a_and_b(self):
         with self.assertRaises(ValueError):
             quadratic_solver(0,0,1)
 
+    ##Tests if the correct value is returned when the quadratic coefficient is zero (and the linear coefficient isn't)
     def test_zero_a(self):
         self.assertListEqual(quadratic_solver(0, 1, 2), [2])
 
+    ##Tests that a single (correct) root is returned when the disriminant is zero
     def test_single_root(self):
         result=quadratic_solver(900,300,25)
 
