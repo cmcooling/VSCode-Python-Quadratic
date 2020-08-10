2259a676081ebf94ea114a3ff4e9448f725b5a58
coolernato <cmcooling@gmail.com>
2020-08-10 10:53:38 +0100

    Updates to quadratic solver

--- quadratic_solver.py
+++ quadratic_solver.py
@@ -1,20 +1,20 @@
-## @package quadratic_solver
-#This package contains a quadratic solver
+'''@package quadratic_solver
+This package contains a quadratic solver'''
 import math
 
-##A solver for quadratic equations in the form ax^2+bx+c=0
-#Will return any roots of the equation in a list
-#If complex roots are allowed, the returned values may be complex
-#If complex roots are not allowed and there are no non-complex roots then a ValueError will be returned
-#If both the quadratic and linear coefficients are zero then a ValueError will be returned
-#@param a (numeric) The quadratic coefficient
-#@param b (numeric) The linear coefficient
-#@param c (numeric) The constant
-#@param complex_allowed (optional) (bool) If True, complex roots will be returned (if appropriate). If False, a ValueError will be returned if the dicrimnant is negative
 def quadratic_solver(a, b, c, complex_allowed=False):
+    '''A solver for quadratic equations in the form ax^2+bx+c=0
+    Will return any roots of the equation in a list
+    If complex roots are allowed, the returned values may be complex
+    If complex roots are not allowed and there are no non-complex roots then a ValueError will be returned
+    If both the quadratic and linear coefficients are zero then a ValueError will be returned'''
+    #@param a (numeric) The quadratic coefficient
+    #@param b (numeric) The linear coefficient
+    #@param c (numeric) The constant
+    #@param complex_allowed (optional) (bool) If True, complex roots will be returned (if appropriate). If False, a ValueError will be returned if the discriminant is negative
     if a==0:
         if b==0:
-            ##If the quadratic and linear coefficiemts are both zero, raise a ValueError
+            ##If the quadratic and linear coefficients are both zero, raise a ValueError
             raise ValueError("Both 'a' and 'b' were zero, meaning there was no defined value for 'x'")
         else:
             ##If the quadratic coefficient is zero, return the constant divided by the linear coefficient
@@ -29,7 +29,7 @@ def quadratic_solver(a, b, c, complex_allowed=False):
            return([(b-1j*math.sqrt(-discriminant))/(2*a), (b+1j*math.sqrt(-discriminant))/(2*a)])
         else:
             ##If the discriminant is negative and complex roots are not allowed, raise a ValueError
-            raise ValueError("The discrimnant was negative and complex reuslts were not allowed")
+            raise ValueError("The discriminant was negative and complex results were not allowed")
     elif discriminant==0:
         ##If the discriminant is zero, return one real root
         return([b/(2*a)])
